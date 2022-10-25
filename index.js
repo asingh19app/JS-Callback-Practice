534f5afd7f5055a8f6f25b393eebc97468f4ab26
Adeshvir Dhanoa <ad13dhanoa@gmail.com>
2022-10-25 17:43:58 -0400

    added js

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const inventory = newInventory()
 move(inventory).to(0, 0)
+//When we are finished, the player should be able to move the character by pressing the arrow keys. In other words, when the user presses the up arrow, the character should move up. If the player then presses the right arrow, the character should switch directions and move right.
 
+//Since the direction of our character will change, we will need to track it with a variable:
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 
-move(newImage('assets/tree.png')).to(200, 450)
+
+//move(newImage('assets/tree.png')).withArrowKeys(200, 450)
+
+
+//Notice we are using another callback function here. The addEventListener receives the inline function we are passing, calls it whenever the event we specify fires, and passes in the e object as an argument.
+
+//Recall "e" is an object containing details about the event that fired.
+
+//Inside our callback function, we will need to check which key was pressed. We can do this using e.key. We will check e.key and, depending on its value, change the character's direction accordingly:
+
+//At this point, you should be able to open the browser, press any of the arrow keys, and see the character start to move across the screen. However, when you release the key, the character probably keeps moving.
+//We need one more event listener to stop the character when the user releases a key:
+
+
+
+
+
+
+
+//move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
--- move.js
+++ move.js
@@ -6,7 +6,71 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left,bottom, callback) {
+        let direction = null
+        //It will also be helpful to keep track of the character's current position with the following variables:
+        let x=left
+        let y = bottom
+        //Now we can think through the logic that would move the character image based on the current direction. For example, if the character is moving west, then x (the distance between the character and the left side of the screen) should decrease:
+        setInterval(function() {
+        if(direction === 'west') {
+            x = x -1
+        }
+        
+        if(direction === 'north') {
+            y = y + 1
+        }
+        
+        if(direction === 'east') {
+            x = x +1
+        }
+        
+        if(direction === 'south') {
+           y = y -1
+        } 
+        
+        //We will finish by using x and y to update the character's left and bottom properties. We are left with the following:
+        element.style.left = x + 'px'
+        element.style.bottom = y +'px'
+        } ,1)
+        //The only real problem with this logic is that it runs once (when the page loads and our character should be standing still). We need it to run repeatedly, so the character moves continuously when the direction is not null.
+        //There is a function in JavaScript called setInterval that can help with this. The setInterval function takes a callback function as its first argument and a certain number of milliseconds as its second argument. It then calls the callback repeatedly, waiting the specified number of milliseconds between each call.
+        
+        //Now that we can move the character by setting the direction variable, let's use event listeners to change the direction whenever the user presses one of the arrow keys. There is a DOM event we can listen for named keydown, which will fire anytime the user presses a key:
+        
+        document.addEventListener('keydown', function(e){
+            if(e.repeat){
+                return
+            }
+                   if(e.key === 'ArrowLeft') {
+                    direction ='west'
+                   }
+        
+                   if(e.key === 'ArrowRight') {
+                    direction ='east'
+                   }
+        
+                   if(e.key === 'ArrowUp') {
+                    direction = 'north'
+                   }
+        
+                   if(e.key === 'ArrowDown') {
+                    direction = 'south'
+                   }
+        
+                 callback(direction)
+        })
+
+        document.addEventListener('keyup' , function(e){
+            direction = null
+        })
+    
+         callback(direction)
+    }
+
+    
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
